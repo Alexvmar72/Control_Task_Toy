9501e610d858255a90b6acc6d4a579c852499732
Alex Mark <alexvmar@yandex.ru>
2023-08-17 15:22:49 +0300

    change2 files toy

--- Toy.java
+++ Toy.java
@@ -1,11 +1,13 @@
 public class Toy {
     private String id;
     private String name;
+    private int amount;
     private int weight;
 
-    public Toy(String id, String name, int weight) {
+    public Toy(String id, String name, int amount, int weight) {
         this.id = id;
         this.name = name;
+        this.amount = amount;
         this.weight = weight;
     }
     public String getId() {
@@ -13,6 +15,9 @@ public class Toy {
     }
     public String getName() {
         return name;
+    }
+    public int getAmount() {
+        return amount;
     }
     public int getWeight() {
         return weight;
