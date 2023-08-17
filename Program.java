670aad156a69eb21e72a490d268159d6385a118c
Alex Mark <alexvmar@yandex.ru>
2023-08-17 15:08:30 +0300

    Initial commit

---
+++ Program.java
@@ -0,0 +1,48 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.PriorityQueue;
+
+public class Program {
+    public static void main(String[] args) {
+        Toy toy1 = new Toy("1", "Car", 5);
+        Toy toy2 = new Toy("2", "Doll", 3);
+        Toy toy3 = new Toy("3", "Ball", 2);
+        Toy toy4 = new Toy("4", "Tank", 7);
+        Toy toy5 = new Toy("5", "Transformer", 4);
+        Toy toy6 = new Toy("6", "Airfly", 6);
+        Toy toy7 = new Toy("7", "Mouse", 1);
+        Toy toy8 = new Toy("8", "Sky", 3);
+        Toy toy9 = new Toy("9", "Constructor", 4);
+        Toy toy10 = new Toy("10", "Game", 2);
+        Toy toy11 = new Toy("11", "Pay", 4);
+        Toy toy12 = new Toy("12", "Mosaic", 1);
+
+        PriorityQueue<Toy> queue = new PriorityQueue<>((t1, t2) -> Integer.compare(t2.getWeight(), t1.getWeight()));
+        queue.add(toy1);
+        queue.add(toy2);
+        queue.add(toy3);
+        queue.add(toy4);
+        queue.add(toy5);
+        queue.add(toy6);
+        queue.add(toy7);
+        queue.add(toy8);
+        queue.add(toy9);
+        queue.add(toy10);
+        queue.add(toy11);
+        queue.add(toy12);
+
+        try {
+            File file = new File("output.txt");
+            FileWriter writer = new FileWriter(file);
+            for (int i = 0; i < 10; i++) {
+                Toy highestPriorityToy = queue.remove();
+                String result = highestPriorityToy.getId() + ": " + highestPriorityToy.getName();
+                writer.write(result + "\n");
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
